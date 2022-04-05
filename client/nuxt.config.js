a49439aea4a6675788985ab4dc8f4b7660c885c8
kennedyb <brian.kennedy@wwt.com>
2022-04-05 16:32:04 -0500

    Update and set favicon and establish Keep Council primary colors

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -10,10 +10,16 @@ export default {
       { name: 'viewport', content: 'width=device-width, viewport-fit=cover, initial-scale=1' },
       { name: 'apple-mobile-web-app-capable', content: 'yes' },
       { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' },
       { hid: 'description', name: 'description', content: 'Helping meeting minute takers more easily organize information into structured data.' },
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
     ],
   },
 
